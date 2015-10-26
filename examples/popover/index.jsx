01e6d5f23bc488912aa3d4c60e660027e690e91a
Beau Collins <beaucollins@gmail.com>
2015-10-26 16:03:18 -0700

    Set up a bunch of buttons for context

--- examples/popover/index.jsx
+++ examples/popover/index.jsx
@@ -32,9 +32,27 @@ const App = React.createClass({
 	},
 
 	render() {
+		const toolbarStyle = {
+			display: 'flex',
+			flexDirection: 'row',
+			justifyContent: 'space-between'
+		};
+		const buttonStyle = {
+			padding: '4px',
+			border: '1px solid #CCC',
+			borderRadius: '2px'
+		};
+		const bottomLeft = Object.assign({}, buttonStyle, { position: 'absolute', left: '0', bottom: '0' });
+		const bottomRight = Object.assign({}, buttonStyle, { position: 'absolute', right: '0', bottom: '0' });
 		return (
 			<div>
-				<a href="#" onClick={this.showPopover}>Click Me</a>
+				<div style={toolbarStyle}>
+					{['Click Me', 'Me Too', 'This One', 'How about this?', 'Me Too'].map((txt, i) => {
+						return (<a href="#" key={i} style={buttonStyle} onClick={this.showPopover}>{txt}</a>);
+					})}
+				</div>
+				<a href="#" style={bottomLeft} onClick={this.showPopover}>Bottom Left</a>
+				<a href="#" style={bottomRight} onClick={this.showPopover}>Bottom Right</a>
 				{this.renderPopover()}
 			</div>
 		);
